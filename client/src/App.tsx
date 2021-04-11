dc0351e3bc12087d474701893fb4db2ea72e0f1c
97Fakhreddine <fakhri19971998@gmail.com>
2021-04-11 21:54:10 +0100

    carrousel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from "react";
 import Nav from "./components/Navbar/Nav";
-import { Button } from "@material-ui/core";
+import Carrousel from "./components/carrousel/Carrousel";
+import Project from "./components/Projects/Projects";
 const App: React.FC<{}> = () => {
   return (
     <div>
       <div>
         <Nav />
+      </div>
+      <div>
+        <Carrousel />
+      </div>
+      <br />
+      <div>
+        <Project />
       </div>
     </div>
   );
--- client/src/components/Navbar/Nav.tsx
+++ client/src/components/Navbar/Nav.tsx
@@ -1,20 +1,34 @@
-import { Button, ButtonGroup } from "@material-ui/core";
 import React from "react";
-
 const Nav: React.FC = () => {
   return (
     <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <div>
-          <div>Welcome To My Portfolio</div>
-          <ButtonGroup
-            variant="contained"
-            aria-label="contained primary button group"
+      <nav className="navbar navbar-expand-lg navbar-light bg-gradient">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <Button>Home</Button>
-            <Button>About</Button>
-            <Button>Contact me</Button>
-          </ButtonGroup>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a className="navbar-brand">Welcome</a>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Contact Me</a>
+              </li>
+            </ul>
+            <div className="d-flex"></div>
+          </div>
         </div>
       </nav>
     </div>

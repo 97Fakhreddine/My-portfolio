d5b5eb9bad72329076e511c3b33b63c849bed0b1
97Fakhreddine <fakhri19971998@gmail.com>
2021-04-12 13:42:26 +0100

    refactoring style

---
+++ client/src/components/backgroundPic/BackgroundPic.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+const BackgroundPic: React.FC = () => {
+  return (
+    <div>
+      <img
+        src="https://images.pexels.com/photos/176851/pexels-photo-176851.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt="background"
+      />
+    </div>
+  );
+};
+
+export default BackgroundPic;
